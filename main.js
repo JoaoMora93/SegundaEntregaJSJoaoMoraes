5392a8ab668d3b32441785b8e97c940a35e34fbc
Joao Moraes <joaomcarril@gmail.com>
2023-05-15 23:02:59 -0300

    primer commit

---
+++ main.js
@@ -0,0 +1,15 @@
+//Objetos
+const persona = [
+    { id: 1, nombre: "Juan", edad: 20, saldo: 10000 },
+    { id: 2, nombre: "Marcela", edad: 33, saldo: 15000 },
+    { id: 3, nombre: "Carlos", edad: 25, saldo: 8000 },
+    { id: 4, nombre: "María", edad: 43, saldo: 3000 },
+  ];
+
+
+const productos = [
+      { id: 1, nombre: "zapato mujer", precio: 1000 },
+      { id: 2, nombre: "camisa", precio: 1350 },
+      { id: 3, nombre: "gorra", precio: 750 },
+      { id: 4, nombre: "media", precio: 250 },
+    ];
