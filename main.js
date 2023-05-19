4d8bdeed0089cd4b14db262466f289a741df12e2
Joao Moraes <joaomcarril@gmail.com>
2023-05-19 14:13:57 -0300

    cargar cliente y mostrar

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 //Objetos
-const persona = [
+const personas = [
     { id: 1, nombre: "Juan", edad: 20, saldo: 10000 },
     { id: 2, nombre: "Marcela", edad: 33, saldo: 15000 },
     { id: 3, nombre: "Carlos", edad: 25, saldo: 8000 },
@@ -13,3 +13,35 @@ const productos = [
       { id: 3, nombre: "gorra", precio: 750 },
       { id: 4, nombre: "media", precio: 250 },
     ];
+
+// Cargar persona nueva
+function cargarPersona() {
+  let id = personas.length + 1;
+  let nombre = prompt("Ingresa el nombre del cliente:")
+  let edad = parseInt(prompt("Ingresa la edad"));
+  let saldo = parseInt(prompt("Ingresa el saldo del cliente:"));
+
+  const persona = {id, nombre, edad, saldo};
+  personas.push(persona);
+}
+
+// Mostrar arreglo de personas
+personas.push(cargarPersona());
+console.log(personas);
+let mensajePersonas = personas;
+mensajePersonas.forEach((persona) => {
+  let mensaje = `
+    Id: ${persona.id}
+    Nombre: ${persona.nombre}
+    Edad: ${persona.edad}
+    Saldo: ${persona.saldo}
+  `;
+  alert(mensaje);
+});
+
+// Mensaje en navegador
+alert (mensajePersonas);
+
+// Menú
+
+
